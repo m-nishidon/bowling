378d0dfddd54f0b0358d243ee3a88ed6b1a9cad1
m-nishidon <m.nishino4127@gmail.com>
2024-08-30 23:59:57 +0900

    add ocr_test

---
+++ pages/ocr_test.py
@@ -0,0 +1,20 @@
+import pytesseract
+import streamlit as st
+from PIL import Image
+
+st.title("画像認識のテスト")
+
+# 画像アップロード
+uploaded_file = st.file_uploader(
+    "スコア表の写真をアップロードしてください", type=["jpg", "png", "jpeg"]
+)
+
+if uploaded_file is not None:
+    # 画像を表示
+    image = Image.open(uploaded_file)
+    st.image(image, caption="Uploaded Image", use_column_width=True)
+
+    # OCR処理
+    st.write("Extracted Text:")
+    text = pytesseract.image_to_string(image, lang="jpn")
+    st.text(text)
