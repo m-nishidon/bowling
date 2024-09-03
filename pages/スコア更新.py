5c0839a22bf3f1cdb4efd8b4aa1fc1420a56c2be
m-nishidon <m.nishino4127@gmail.com>
2024-09-04 01:22:19 +0900

    score update ini3

--- pages/スコア更新.py
+++ pages/スコア更新.py
@@ -10,17 +10,28 @@ df, df_team, current_frame, df_conf = utils.read_origin_score()
 df = df[df.columns[:-21]]
 
 
-def select(df, colname, multi):
-    selected_elements = "ALL"
+def select(df, colname, multi, selected_past):
+    if not selected_past:
+        selected_elements = "ALL"
+
     st.subheader(f"{colname}選択")
     labels = ["ALL"] + sorted(df[colname].unique())
 
     if not multi:
+        if selected_past:
+            selected_item = selected_past.pop()
+            idx = labels.index(selected_item)
+        else:
+            idx = 0
         selected_elements = set()
-        selected_elements.add(st.selectbox(f"{colname}を選択してください", labels))
+        selected_elements.add(st.selectbox(f"{colname}を選択してください", labels, idx))
     else:
         selected_elements = set(
-            st.multiselect(f"{colname}を選択してください", labels, labels[0])
+            st.multiselect(
+                f"{colname}を選択してください",
+                labels,
+                selected_past if selected_past else labels[0],
+            )
         )
 
     if "ALL" not in selected_elements:
@@ -28,11 +39,21 @@ def select(df, colname, multi):
     return df, selected_elements
 
 
-df, _ = select(df, "拠点", False)
-df, _ = select(df, "チーム", True)
-df, selected_elements = select(df, "名前", True)
+selected_items = []
+for key in ["area", "team", "name"]:
+    if key in st.session_state:
+        selected_items.append(st.session_state[key])
+    else:
+        selected_items.append(None)
+selected_area, selected_team, selected_name = selected_items
+
+df, st.session_state["area"] = select(df, "拠点", False, selected_area)
+df, st.session_state["team"] = select(df, "チーム", True, selected_team)
+df, st.session_state["name"] = select(df, "名前", True, selected_name)
 st.subheader("ゲーム選択")
-selected_game = st.selectbox("何ゲーム目かを選択してください", (1, 2))
+idx = st.session_state["game"] if "game" in st.session_state else 0
+selected_game = st.selectbox("何ゲーム目かを選択してください", (1, 2), idx)
+st.session_state["game"] = selected_game - 1
 if selected_game == 1:
     df = df[df.columns[:-21]]
 else:
@@ -49,4 +70,7 @@ else:
     ]
 edited_df = st.data_editor(df)
 
-st.write("前回のフィルター条件の保存、変な数字の確認、2ゲーム目しない場合")
+
+st.write(
+    "前回のフィルター条件の保存(バグってます…)、変な数字の確認、2ゲーム目しない場合"
+)
